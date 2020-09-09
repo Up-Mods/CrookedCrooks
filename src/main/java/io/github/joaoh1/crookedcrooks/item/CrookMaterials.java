bc83b07c5f7b08900654577a0cd5ebcac9af0ab3
Past Ennui <dont_worry.you_will_escape_this_darkness@example.com>
2020-09-08 22:54:31 -0300

    Common tag all things! Also: Start to add more Astromine crooks

--- src/main/java/io/github/joaoh1/crookedcrooks/item/CrookMaterials.java
+++ src/main/java/io/github/joaoh1/crookedcrooks/item/CrookMaterials.java
@@ -43,7 +43,7 @@ public enum CrookMaterials implements ToolMaterial {
 		return Ingredient.ofItems(Items.NETHER_BRICK);
 	}),
 	//Astromine Materials
-	METITE(2, 981, 14.0F, 5.0F, 5, () -> {
+	METITE(1, 853, 13.0F, 4.0F, 5, () -> {
 		return Ingredient.fromTag(TagRegistry.item(new Identifier("c:metite_ingots")));
 	}),
 	ASTERITE(5, 2015, 10.0F, 5.0F, 20, () -> {
@@ -58,7 +58,7 @@ public enum CrookMaterials implements ToolMaterial {
 	UNIVITE(7, 3918, 12.0F, 6.0F, 22, () -> {
 		return Ingredient.fromTag(TagRegistry.item(new Identifier("c:univite_ingots")));
 	}),
-	LUNUM(4, 1382, 7f, 4.5f, 18, () -> {
+	LUNUM(4, 1382, 7.0F, 4.5F, 18, () -> {
 		return Ingredient.fromTag(TagRegistry.item(new Identifier("c:lunum_ingots")));
 	}),
 	COPPER(1, 200, 4.0F, 1.5F, 10, () -> {
@@ -71,10 +71,25 @@ public enum CrookMaterials implements ToolMaterial {
 		return Ingredient.fromTag(TagRegistry.item(new Identifier("c:silver_ingots")));
 	}),
 	LEAD(2, 496, 4.5F, 1.5F, 5, () -> {
-		return Ingredient.fromTag(TagRegistry.item(new Identifier("c:tin_ingots")));
+		return Ingredient.fromTag(TagRegistry.item(new Identifier("c:lead_ingots")));
 	}),
 	ASTROMINE_BRONZE(2, 539, 7.0F, 2.5F, 18, () -> {
-		return Ingredient.fromTag(TagRegistry.item(new Identifier("c:lead_ingots")));
+		return Ingredient.fromTag(TagRegistry.item(new Identifier("c:bronze_ingots")));
+	}),
+	STEEL(3, 1043, 7.5F, 3.0F, 16, () -> {
+		return Ingredient.fromTag(TagRegistry.item(new Identifier("c:steel_ingots")));
+	}),
+	ELECTRUM(2, 185, 11.0F, 1.0F, 21, () -> {
+		return Ingredient.fromTag(TagRegistry.item(new Identifier("c:electrum_ingots")));
+	}),
+	ROSE_GOLD(1, 64, 10.0F, 0.5F, 24, () -> {
+		return Ingredient.fromTag(TagRegistry.item(new Identifier("c:rose_gold_ingots")));
+	}),
+	STERLING_SILVER(2, 697, 7.0F, 2.5F, 20, () -> {
+		return Ingredient.fromTag(TagRegistry.item(new Identifier("c:sterling_silver_ingots")));
+	}),
+	FOOLS_GOLD(2, 250, 6.5F, 2.0F, 16, () -> {
+		return Ingredient.fromTag(TagRegistry.item(new Identifier("c:fools_gold_ingots")));
 	}),
 	//Applied Energistics 2 Materials
 	CERTUS_QUARTZ(ToolMaterials.IRON.getMiningLevel(), ToolMaterials.IRON.getDurability(), ToolMaterials.IRON.getMiningLevel(), ToolMaterials.IRON.getAttackDamage(), ToolMaterials.IRON.getEnchantability(), () -> {
